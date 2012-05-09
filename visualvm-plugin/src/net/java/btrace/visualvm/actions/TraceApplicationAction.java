277625026b235dde397c0aa6651ec9c69d5d7ba7
Jarda Bachorik <j.bachorik@gmail.com>
2012-05-09 14:26:33 +0200

    #BTRACE-84: Updating the VisualVM plugin to allow tracing applications running on JDK7

--- visualvm-plugin/src/net/java/btrace/visualvm/actions/TraceApplicationAction.java
+++ visualvm-plugin/src/net/java/btrace/visualvm/actions/TraceApplicationAction.java
@@ -34,7 +34,7 @@ public class TraceApplicationAction extends SingleDataSourceAction<Application> 
     @Override
     protected boolean isEnabled(Application app) {
         Jvm jvm = JvmFactory.getJVMFor(app);
-        return app.isLocalApplication() && jvm.isAttachable() && jvm.isGetSystemPropertiesSupported() && !jvm.is17();
+        return app.isLocalApplication() && jvm.isAttachable() && jvm.isGetSystemPropertiesSupported();
     }
 
     public static synchronized TraceApplicationAction newInstance() {
